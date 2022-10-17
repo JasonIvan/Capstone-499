d45ad2366111716716d17bf680f3ddddedce8c7c
JasonIvan <95385642+JasonIvan@users.noreply.github.com>
2022-10-17 14:49:35 -0400

    Add files via upload

---
+++ HowItWorks.js
@@ -0,0 +1,7 @@
+// HowItWorks.js
+
+/*
+
+For our Cars 4 U Website, we are using React.js incoroporated with Django and python to read and extract .csv files from car datasets. This allows us to have access to many different caars with different attributes from various datasets. We are also using .json files to fetch the .csv file and convert it so that the uer has all the data and information needed for when they search what kind of car they want. It is filtered by milage, whether the car is combustion engine or electric, make of model, and so on. We actually connect the user to the datbase itself through Django and extrct through React so it's user friendly. The front end part of the website uses React and has a navbar the allows the user to find what they need and filter anthing else out. This makes it easier when getting a car.
+
+*/
